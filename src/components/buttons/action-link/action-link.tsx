0dc38e1e094426eb237432b0e42da726a44fa812
vanderms <vanderms.84@gmail.com>
2022-07-22 10:47:12 -0300

    implemented call to actiomn section responsive design

--- src/components/buttons/action-link/action-link.tsx
+++ src/components/buttons/action-link/action-link.tsx
@@ -2,14 +2,21 @@ interface ActionLinkProps {
   href: string;
   children: React.ReactNode;
   className?: string;
+  negative?: boolean;
 }
 
 export const ActionLink: React.FC<ActionLinkProps> = (props) => {
   return (
     <a
       href={props.href}
-      className={`text-[0.9375rem] h-11 w-max px-8 grid place-items-center shadow-[0px_15px_15px_-10px_#FF9F8E] 
-        rounded-full text-neutral-50 bg-primary font-bold hover:bg-primary-300  ${props.className ?? ""}`}
+      className={`text-[0.9375rem] h-11 w-max px-8 grid place-items-center     
+      rounded-full  font-bold 
+      ${
+        props.negative
+          ? "text-primary bg-neutral-50 hover:text-primary-300 shadow-[0px_15px_15px_-10px_#DB5943;]"
+          : "text-neutral-50 bg-primary hover:bg-primary-300  shadow-[0px_15px_15px_-10px_#FF9F8E] "
+      }
+      ${props.className ?? ""}`}
     >
       {props.children}
     </a>
--- src/components/sections/call-to-action/call-to-action.tsx
+++ src/components/sections/call-to-action/call-to-action.tsx
@@ -1,11 +1,22 @@
+import { ActionLink } from "@/components/buttons/action-link/action-link";
 import { useId } from "react";
 
 export const CallToActionSection: React.FC = () => {
   const title = useId();
 
   return (
-    <section aria-labelledby={title}>
-      <h2 id={title}>Call to Action</h2>
+    <section
+      aria-labelledby={title}
+      className="mt-10 ctn bg-primary py-24 relative call-to-action-pattern xl:flex xl:justify-between xl:items-center xl:pt-[3.875rem] xl:pb-[4.375rem] xl:mt-[11.25rem]"
+    >
+      <h2 id={title} className="title-lg text-neutral-50 text-center max-w-[27.8125rem] xl:text-left">
+        Simplify how your team works today.
+      </h2>
+      <div className="relative z-10 mt-7 flex justify-center">
+        <ActionLink negative={true} href="/">
+          Get Started
+        </ActionLink>
+      </div>
     </section>
   );
 };
